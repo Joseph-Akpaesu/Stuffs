4f9aef788c78ba7b440e1b1ddde3e4d42dec3284
Joseph-Akpaesu <joejimmy230@gmail.com>
2021-09-29 12:39:37 +0100

    complete fodie

--- pages/login.vue
+++ pages/login.vue
@@ -1,151 +1,202 @@
+
 <template>
   <div>
-    <div class='nav-bar' style="position: absolute; z-index: 1; padding: 20px 0px 0px 40px;">
-    <ul>
-        <li>
-            <a href="#"><img src="~/assets/img/Group 10.png" alt="" width="50px"></a>
-        </li>
-    </ul>
-    </div>
-    <section>
-      <a-row>
-      <a-col :sm="{ span: 6, offset: 10}" :lg="{ span: 10, offset: 0 }">
-      <div class="img-container">
-         <img src="~/assets/img/Group 113.png" alt="" width="520px" height="688px">
-      </div>
-    </a-col>
-    <a-col :sm="{ span: 5, offset: 5}" :lg="{ span: 10, offset: 2}">
-    <div class="sign-up-text">
-      <h3>Login</h3>
-      <p>Don't have an account? <a href="#">signup </a></p>
-    <a-button type="" id="google-signup-btn">
-      <a href="#">Sign in with google</a>
-      <i class="fab fa-google google" style="color: black; font-size: 20px;"></i>
-    </a-button>
-    <a-button type="" id="facebook-signup-btn">
-      <a href="#">Sign in with facebook</a>
-      <i class="fab fa-facebook-square" style="color: black; font-size: 20px;"></i>
-    </a-button>
-    <div style="display: flex;" class="alternative-container">
-      <hr style="width:40%; background-color: #bf0505; margin: 41px 0px 0px 70px;">
-      <p id="alternative">Or</p>
-      <hr style="width:40%; background-color: #bf0505; margin-top: 41px; margin-left: 19px;" >
-    </div>
-    </div>
-    </a-col>
-    <a-col :sm="{ span: 5, offset: 5}" :lg="{ span: 10, offset: 2}">
-    <a-form
-    id="components-form-demo-normal-login"
-    :form="form"
-    @submit="handleSubmit">
-    <a-form-item v-bind="formItemLayout">
-      <a-input
-        v-decorator="[
-          'email',
-          {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          },
-        ]"
-        placeholder="Email"
-        >
-      <a-icon slot="prefix" type="message" style="color: rgba(0,0,0,.25)"/>
-      </a-input>
-    </a-form-item>
-    <a-form-item>
-      <a-input
-        v-decorator="[
-          'password',
-          { rules: [{ required: true, message: 'Please input your Password!' }] },
-        ]"
-        type="password"
-        placeholder="Password"
-        >
-        <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item >
-      <a href="#" class="login-form-forgot">
-        Forgot password
-      </a><br>
-      <a-button type="" html-type="submit" class="login-form-button">
-        <a href="#">Login</a>
-      </a-button>
-    </a-form-item>
-  </a-form>
-    </a-col>
-    </a-row>
+    <section class="nav-bar">
+      <ul>
+          <li class="img-1">
+              <a href="#"><img src="~/assets/img/Group 10.png" alt="" width="50px"></a>
+          </li>
+          <li class="img-2">
+            <a href="#"><img src="~/assets/img/Group 101.png" alt="" width="50px"></a>
+          </li>
+      </ul>
     </section>
+
+    <a-row>
+      <a-col :sm="{ span: 5, offset: 1 }" :lg="{ span: 6, offset: 0 }">
+        <img id="img" src="~/assets/img/Group 113.png" alt="" width="520px" height="688px">
+      </a-col>
+
+      <a-col class="login-text" :sm="{ span: 10, offset: 5}" :lg="{ span: 10, offset: 5}">
+        <h3>Login</h3>
+        <p>Don't have an account? <a href="#">signup </a></p>
+
+        <a-col :sm="{ span: 5, offset: 1 }" :lg="{ span: 6, offset: 2 }">
+          <a-button type="" id="google-signup-btn">
+            <a href="#">Sign in with google</a>
+            <i class="fab fa-google google" style="color: black; font-size: 20px; padding-left: 10px;"></i>
+          </a-button>
+        </a-col>
+
+        <a-col :sm="{ span: 10, offset: 5 }" :lg="{ span: 10, offset: 5 }">
+          <a-button type="" id="facebook-signup-btn">
+            <a href="#" >Sign in with facebook</a>
+            <i class="fab fa-facebook-square" style="color: black; font-size: 20px;  padding-left: 10px;"></i>
+          </a-button>
+        </a-col>
+        <a-divider class="Divider">Or</a-divider>
+      </a-col>
+
+      <a-col class="login-form" :sm="{ span: 5, offset: 1 }" :lg="{ span: 9, offset: 6 }">
+        <a-form
+        id="components-form-demo-normal-login"
+        :form="form"
+        class="login-form"
+        @submit="handleSubmit"
+      >
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'userName',
+              { rules: [{ required: true, message: 'Please input your username!' }] },
+            ]"
+            placeholder="Username"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              { rules: [{ required: true, message: 'Please input your Password!' }] },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a class="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <a-button type="primary" html-type="submit" class="login-form-button">
+            <a href="#">Log in</a>
+          </a-button>
+        </a-form-item>
+        </a-form>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
-  import utils from '@/mixins/utils'
   export default {
-    layout: 'plain',
-    name: 'login',
-    mixins: [utils],
-
-    beforeMount() {
-
-    },
-
+      layout: 'plain',
     beforeCreate() {
-    this.form = this.$form.createForm(this, { name: 'normal_login' });
-},
-
-    methods: {
-      handleSubmit(e) {
-        e.preventDefault();
-        this.form.validateFields((err, values) => {
-          if (!err) {
-            console.log('Received values of form: ', values);
-          }
-        });
-      },
-    },
-  };
-
+   this.form = this.$form.createForm(this, { name: 'normal_login' });
+ },
+ methods: {
+   handleSubmit(e) {
+     e.preventDefault();
+     this.form.validateFields((err, values) => {
+       if (!err) {
+         console.log('Received values of form: ', values);
+       }
+     });
+   },
+ },
+  }
 </script>
 
 <style scoped>
-
 .nav-bar {
-width: 100%;
+  width: 100%;
+  position: absolute;
+  padding: 20px 0px 0px 40px;
+  z-index: 1
 }
 
-.nav-bar ul {
-display: flex;
+.nav-bar ul{
+  display: flex;
 }
 
 .nav-bar li {
-list-style: none;
+  list-style: none;
 }
 
-#components-form-demo-normal-login{
-    /* margin: 45px 0 0 0; */
+.nav-bar .img-2 {
+  display: none;
 }
 
-.login-form-button {
-    background: #bf0505;
-    color: white;
-    padding: 10px 0px 30px 0px;
-    max-width: 150px;
-    border-radius: 20px;
-    border: none;
-    font-weight: 800;
-    font-size: 1.1em;
-    margin-left: 190px;
-    margin-top: 29px;
-    width: 50%;
+.login-text {
+  text-align: center;
+  margin-top: 120px;
+}
+
+.login-text h3 {
+  font-size: 2.4em;
+  font-weight: 900;
+}
+
+.login-text a {
+  color: #bf0505;
+  text-decoration: none;
+}
+
+.login-text p {
+  color: rgba(0,0,0,.50);
+}
+
+#google-signup-btn  {
+  background-color: white;
+  box-shadow: 0 2px 5px 0 black;
+  border: 1px solid white;
+  border-radius: 10px;
+  padding: 12px 10px 31px 20px;
+}
+
+#google-signup-btn a {
+  color: rgba(0,0,0,.50);
+  text-decoration: none;
+}
+
+#facebook-signup-btn  {
+  background-color: skyblue;
+  box-shadow: 0 2px 5px 0 black;
+  border: 1px solid skyblue;
+  border-radius: 10px;
+  padding: 12px 10px 31px 20px;
+}
+
+#facebook-signup-btn a {
+  color: white;
+  text-decoration: none;
+}
+
+.Divider {
+  margin-top: 15%;
+}
+
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+
+.login-form {
+  margin-top: 25px;
+}
+
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+  color: #bf0505;
+}
+
+.login-form-forgot:hover {
+  text-decoration: none;
+}
+
+#components-form-demo-normal-login .login-form-button {
+  width: 36%;
+  margin-top: 50px;
+  background: #bf0505;
+  color: white;
+  padding: 10px 0px 30px 0px;
+  border-radius: 20px;
+  border: none;
+  font-weight: 800;
+  font-size: 1.1em;
+  margin-left: 31%;
 }
 
 .login-form-button:hover{
@@ -159,97 +210,20 @@ list-style: none;
   letter-spacing: 1px;
 }
 
-.sign-up-text{
-   text-align: center;
-   margin: 82px 0 0 0px;
-}
-.sign-up-text a {
-    color: #bf0505;
-    text-decoration: none;
-}
-
-.sign-up-text p {
-  color: rgba(0,0,0,.50);
-}
-
-.sign-up-text h3 {
-    font-size: 2.4em;
-    font-weight: 900;
-}
-
-#google-signup-btn  {
-    background-color: white;
-    box-shadow: 0 2px 5px 0 black;
-    border: 1px solid white;
-    border-radius: 10px;
-    padding: 12px 10px 31px 20px;
-    margin: 20px 35px 0px 0px;
-}
-
-#google-signup-btn a {
-  color: rgba(0,0,0,.50);
-}
-
-#facebook-signup-btn  {
-    background-color: skyblue;
-    box-shadow: 0 2px 5px 0 black;
-    border: 1px solid skyblue;
-    border-radius: 10px;
-    padding: 12px 10px 31px 20px;
-    margin: 20px 35px 0px 0px;
-}
-
-#facebook-signup-btn a {
-  color: white;
-  font-size: ;
-  font-weight: 0;
-  padding: 0 20px 0 4px;
-}
-
-#alternative {
-  margin: 27px 0px 0px 13px;
-}
-
-.google{
-    padding: 0 10px 0 20px;
-}
-
-.login-form-forgot {
-    color: #bf0505;
-    float: right;
-    text-decoration: none;
-}
-
-.login-form-forgot:hover {
-    text-decoration: none;
-    color: #bf0505;
-}
-
 @media only screen and (max-width: 600px) {
-  #components-form-demo-normal-login {
-    margin-left: 35px;
-    width: 80%;
+  .nav-bar .img-1 {}
+
+  .nav-bar .img-2 {
+    display: none;
   }
-  .img-container img {
+
+  #img{
     width: 100%;
-    height: 100%;
+    height: auto;
   }
-.sign-up-text {
-  margin-left: 40px;
-}
 
-.login-form-button {
-  margin-left: 52px;
-}
-
-.login-form-forgot {
-  margin-right: 10px;
-}
-
-.alternative-container hr {
-  margin-left: 90;
-
-}
-
+  #facebook-signup-btn {
+  margin-top: 5%;
+  }
 }
 </style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,101 +1,104 @@
+
 <template>
-<div>
+  <div>
     <div class='nav-bar' style="position: absolute; z-index: 1; margin: 20px 0px 0px 40px;">
-    <ul>
+      <ul>
         <li>
-            <a href="#"><img src="~/assets/img/Group 10.png" alt="" width="50px"></a>
+          <a href="#"><img src="~/assets/img/Group 10.png" alt="" width="50px"></a>
         </li>
-    </ul>
+      </ul>
     </div>
+
     <section>
-    <a-row>
-    <a-col :sm="{ span: 6, offset: 10}" :lg="{ span: 10, offset: 0 }">
-    <div>
-       <img src="~/assets/img/Group 113.png" alt="" width="520px" height="688px">
-    </div>
-    </a-col>
-    <a-col :sm="{ span: 11, offset: 10}" :lg="{ span: 8, offset: 5}">
-    <div class="sign-up-text">
-      <h3>Sign-up</h3>
-      <p>Already hava an account? <a href="#">login</a></p>
-    <a-button type="" id="google-signup-btn">
-      <a href="#">Sign up with google</a>
-      <i class="fab fa-google google" style="color: black;"></i>
-    </a-button>
-    </div>
-    <a-form
-    id="components-form-demo-normal-login"
-    :form="form"
-    @submit="handleSubmit">
-    <a-form-item>
-      <a-input
-        v-decorator="[
-          'userName',
-          { rules: [{ required: true, message: 'Please input your username!' }] },
-        ]"
-        placeholder="Username"
-      >
-        <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item v-bind="formItemLayout">
-      <a-input
-        v-decorator="[
-          'email',
-          {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          },
-        ]"
-        placeholder="Email"
-      >
-      <a-icon slot="prefix" type="message" style="color: rgba(0,0,0,.25)"/>
-      </a-input>
-    </a-form-item>
-    <a-form-item>
-      <a-input
-        v-decorator="[
-          'password',
-          { rules: [{ required: true, message: 'Please input your Password!' }] },
-        ]"
-        type="password"
-        placeholder="Password"
-      >
-        <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item >
-      <a-checkbox
-        v-decorator="[
-          'remember',
-          {
-            valuePropName: 'checked',
-            initialValue: true,
-          },
-        ]"
-      class="login-form-agreement" >
-           I agree to the terms of service and privacy policy
-      </a-checkbox><br>
-      <!--<a href="#" class="login-form-forgot">
-        Forgot password
-      </a>-->
-      <a-button type="" html-type="submit" class="login-form-button">
-        <a href="#">Register</a>
-      </a-button>
-    </a-form-item>
-  </a-form>
-    </a-col>
-    </a-row>
+      <a-row>
+        <a-col :sm="{ span: 20, offset: 10}" :lg="{ span: 10, offset: 0 }">
+          <div class="img-container">
+            <img src="~/assets/img/Group 113.png" alt="" width="520px" height="688px">
+          </div>
+        </a-col>
+
+        <a-col :sm="{ span: 5, offset: 5}" :lg="{ span: 10, offset: 2}">
+          <div class="sign-up-text">
+            <h3>Sign-up</h3>
+            <p>Already hava an account? <a href="#">login</a></p>
+            <a-button type="" id="google-signup-btn">
+              <a href="#">Sign up with google</a>
+              <i class="fab fa-google google" style="color: black; font-size: 20px; padding-left: 10px"></i>
+            </a-button>
+          </div>
+        </a-col>
+
+        <a-col :sm="{ span: 5, offset: 5}" :lg="{ span: 10, offset: 2}">
+          <a-form
+          id="components-form-demo-normal-login"
+          :form="form"
+          @submit="handleSubmit">
+          <a-form-item>
+            <a-input
+              v-decorator="[
+                'userName',
+                { rules: [{ required: true, message: 'Please input your username!' }] },
+              ]"
+              placeholder="Username"
+            >
+              <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+            </a-input>
+          </a-form-item>
+          <a-form-item v-bind="formItemLayout">
+            <a-input
+              v-decorator="[
+                'email',
+                {
+                  rules: [
+                    {
+                      type: 'email',
+                      message: 'The input is not valid E-mail!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your E-mail!',
+                    },
+                  ],
+                },
+              ]"
+              placeholder="Email"
+            >
+            <a-icon slot="prefix" type="message" style="color: rgba(0,0,0,.25)"/>
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              v-decorator="[
+                'password',
+                { rules: [{ required: true, message: 'Please input your Password!' }] },
+              ]"
+              type="password"
+              placeholder="Password"
+            >
+              <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)"/>
+            </a-input>
+          </a-form-item>
+          <a-form-item >
+            <a-checkbox
+              v-decorator="[
+                'remember',
+                {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                },
+              ]"
+            class="login-form-agreement" >
+                 I agree to the terms of service and privacy policy
+            </a-checkbox><br>
+            <a-button type="" html-type="submit" class="login-form-button">
+              <a href="#">Register</a>
+            </a-button>
+          </a-form-item>
+        </a-form>
+        </a-col>
+      </a-row>
     </section>
-</div>
+  </div>
 </template>
 
 <script>
@@ -117,9 +120,8 @@
 };
 </script>
 
-<style >
+<style>
 <style scoped>
-
 .nav-bar {
 width: 100%;
 }
@@ -134,9 +136,9 @@ list-style: none;
 
 
 #components-form-demo-normal-login{
-    margin: 75px 70px 0 -150px;
-
+    margin-top: 50px;
 }
+
 .login-form-button {
     background: #bf0505;
     color: white;
@@ -163,8 +165,9 @@ list-style: none;
 
 .sign-up-text{
    text-align: center;
-   margin: 80px 0 0 -200px;
+   margin-top: 100px;
 }
+
 .sign-up-text a {
     color: #bf0505;
     text-decoration: none;
@@ -185,30 +188,31 @@ list-style: none;
     border: 1px solid white;
     border-radius: 10px;
     padding: 12px 10px 31px 20px;
-    margin: 20px 0 0 0;
+    margin-top: 20px;
 }
 
 #google-signup-btn a {
   color: rgba(0,0,0,.50);
 }
 
-.google{
-    padding: 0 10px 0 20px;
-}
-
-/*.login-form-forgot {
-    color: #bf0505;
-    float: right;
-    text-decoration: none;
-}
-
-.login-form-forgot:hover {
-    text-decoration: none;
-    color: #bf0505;
-}*/
-
 .login-form-agreement {
   color: #bf0505;
 }
 
+@media only screen and (max-width: 600px) {
+  .img-container img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .login-form-button {
+    margin-left: 33%;
+    margin-bottom: 40px;
+  }
+
+  #components-form-demo-normal-login{
+    width: 100%;
+    margin-top: 40px;
+  }
+}
 </style>
